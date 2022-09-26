c7b4277d33fb051fb2a5a7a5f9f3b857a897ba5d
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-09-26 11:56:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanjeZadatak9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>() { 6, 37, 25, 13, 1, 75, 7 };
+            lista = lista.OrderBy(i => i).ToList();
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
